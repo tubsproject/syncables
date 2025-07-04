f8a7efd011ff9db11240b9d2a7f9e741d37d6f10
Michiel de Jong <michiel@pondersource.com>
2025-07-04 13:03:02 +0200

    update

--- build/src/lens.js
+++
@@ -1,9 +0,0 @@
-export class DateLens {
-    forward(s) {
-        return new Date(s);
-    }
-    backward(d) {
-        return d.toString();
-    }
-}
-//# sourceMappingURL=lens.js.map
--- build/src/testTracker.js
+++
@@ -1,9 +0,0 @@
-import { getFetcher } from './solid/fetcher.js';
-import { fetchTracker } from './solid/tasks.js';
-async function test() {
-    const fetcher = await getFetcher();
-    fetchTracker(process.env.TRACKER_IN_SOLID, fetcher);
-}
-//...
-test();
-//# sourceMappingURL=testTracker.js.map
