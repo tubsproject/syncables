8e9a98eaae4d9d473a4abda82ba6b50d261a0346
Michiel de Jong <michiel@pondersource.com>
2025-10-13 17:37:52 +0200

    A-Cube, Peppyrus, Ion, Google Calendar all working

--- src/client.ts
+++ src/client.ts
@@ -4,10 +4,12 @@ export async function fetchData(
   endPoint: string,
   authHeaders: { [key: string]: string },
 ): Promise<object> {
-  // console.log(`Fetching data from ${endPoint} with headers:`, authHeaders);
+  console.log(`Fetching data from ${openApiSpec.servers[0].url + endPoint} with headers:`, authHeaders);
   const url = openApiSpec.servers[0].url + endPoint; // + '?key=' + process.env.GOOGLE_API_KEY;
   const res = await fetch(url, {
-    headers: authHeaders,
+    headers: Object.assign({}, authHeaders, {
+      'Content-Type': 'application/json',
+    }),
   });
   // console.log(`Response status: ${res.status}`, await res.text());
   return await res.json();
