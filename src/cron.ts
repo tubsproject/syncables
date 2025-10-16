c8b3f556fdf31e5fc84d6253751c694abd4d9e86
Michiel de Jong <michiel@pondersource.com>
2025-10-16 13:01:58 +0200

    Debugging cron script for LetsPeppol proxy as an example usage here

---
+++ src/cron.ts
@@ -0,0 +1,57 @@
+import { getPostgresClient, Client } from './db.js';
+import { Syncable } from './main.js';
+
+async function createCollections(
+  collectionName: string,
+  client: Client,
+): Promise<void> {
+  const openApiSpecFilename = `openapi/generated/${collectionName}.yaml`;
+  const envKey = `${collectionName.toUpperCase().replace('-', '_')}_AUTH_HEADERS`;
+  if (!process.env[envKey]) {
+    console.warn(`Skipping ${collectionName} because ${envKey} is not set`);
+    return;
+  }
+  console.log(`Creating collection for ${collectionName} using ${openApiSpecFilename}`);
+  const authHeaders: { [key: string]: string } = JSON.parse(process.env[envKey]);
+  const syncable = new Syncable(
+    collectionName,
+    openApiSpecFilename,
+    authHeaders,
+    client,
+  );
+  await syncable.init();
+  await syncable.run();
+}
+
+async function run(): Promise<void> {
+  const client = await getPostgresClient();
+  // get list of backend platforms from env vars
+  const platformsList = Object.keys(process.env)
+    .filter((x) => x.endsWith('_AUTH_HEADERS'))
+    .map((x) =>
+      x
+        .substring(0, x.length - '_AUTH_HEADERS'.length)
+        .toLowerCase()
+        .replace('_', '-'),
+    );
+  console.log('Platforms to sync:', platformsList);
+  await Promise.all(
+    platformsList.map((platform) => createCollections(platform, client)),
+  );
+  await client.end();
+}
+
+// ...
+run();
+
+// ...
+// runOAuthClient({
+//   authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
+//   tokenURL: 'https://oauth2.googleapis.com/token',
+//   clientID: process.env.GOOGLE_CLIENT_ID || ((): void => { throw new Error('GOOGLE_CLIENT_ID not set') })(),
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET || ((): void => { throw new Error('GOOGLE_CLIENT_SECRET not set') })(),
+//   callbackURL: 'http://localhost:8000/callback'
+// }, 8000, async (token) => {
+//   console.log(`Received OAuth token: ${token}`);
+// }); // Start the OAuth client on port 8000
+// console.log('Data fetched and inserted. Visit http://localhost:8000/ if you need to renew the GOOGLE_OAUTH_TOKEN env var.');
