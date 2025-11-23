965fc5877e46e928275246d9e4d0d3a4658ed8e2
Michiel de Jong <michiel@pondersource.com>
2025-11-23 13:23:55 +0100

    Add unit test for pageToken-based paging

--- __tests__/unit/paging.test.ts
+++ __tests__/unit/paging.test.ts
@@ -1,7 +1,7 @@
 import { test, vi, expect } from 'vitest';
 import { Syncable } from '../../src/syncable.js';
 
-test('fetches data successfully from API', async () => {
+test('pageNumber paging', async () => {
   // Mock the fetch function.
   const mockResponses = [
     {
@@ -37,4 +37,42 @@ test('fetches data successfully from API', async () => {
   expect(fetchMock).toHaveBeenCalledTimes(2);
   expect(fetchMock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/?page=1');
   expect(fetchMock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/?page=2');
+});
+
+test('pageToken paging', async () => {
+  // Mock the fetch function.
+  const mockResponses = [
+    {
+      items: [
+        { id: 1, title: 'Test Todo 1' },
+        { id: 2, title: 'Test Todo 2' },
+      ],
+      nextPageToken: 'token1',
+    },
+    {
+      items: [
+        { id: 1, title: 'Test Todo 3' },
+      ],
+      nextPageToken: null,
+    },
+  ];
+  let index = 0;
+  const fetchMock = vi.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockResponses[index++]),
+    }),
+  );
+
+  // Call the function and assert the result
+  const syncable = new Syncable({
+    pagingStrategy: 'pageToken',
+    listUrl: 'https://jsonplaceholder.typicode.com/todos/',
+  }, fetchMock as unknown as typeof fetch);
+  const data = await syncable.fullFetch();
+  expect(data).toEqual(mockResponses[0].items.concat(mockResponses[1].items));
+
+  // Check that fetch was called exactly once
+  expect(fetchMock).toHaveBeenCalledTimes(2);
+  expect(fetchMock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/');
+  expect(fetchMock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/?pageToken=token1');
 });
