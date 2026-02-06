c3b2305a307664c405c758e3ffa7b9620fe7f5cc
Michiel de Jong <michiel@pondersource.com>
2026-02-06 17:45:01 +0700

    prettier

--- __tests__/integration/mock-server/apply-pagination.ts
+++ __tests__/integration/mock-server/apply-pagination.ts
@@ -11,42 +11,48 @@ export function applyPagination(
   let numItems = 1;
   let hasMore = false;
   switch (spec.pagingStrategy) {
-    case 'pageNumber': {
-      let pageNum = parseInt(query[spec.pageNumberParamInQuery ?? 'page'], 10);
-      if (isNaN(pageNum) || pageNum < 1) {
-        pageNum = 1;
+    case 'pageNumber':
+      {
+        let pageNum = parseInt(
+          query[spec.pageNumberParamInQuery ?? 'page'],
+          10,
+        );
+        if (isNaN(pageNum) || pageNum < 1) {
+          pageNum = 1;
+        }
+        numItems = pageNum < 4 ? 10 : pageNum == 4 ? 7 : 0;
+        hasMore = pageNum < 4;
       }
-      numItems = pageNum < 4 ? 10: pageNum == 4 ? 7 : 0;
-      hasMore = pageNum < 4;
-    }
-    break;
-    case 'pageToken': {
-      const pageToken = query[spec.pageTokenParamInQuery ?? 'pageToken'];
-      // console.log('serving pageToken', pageToken);
-      let page = 1;
-      if (!pageToken) {
-        numItems = 10;
-        hasMore = true;
-      } else if (pageToken === 'token-2') {
-        page = 2;
-        numItems = 10;
-        hasMore = true;
-      } else if (pageToken === 'token-3') {
-        page = 3;
-        numItems = 5;
-      } else {
-        numItems = 0;
+      break;
+    case 'pageToken':
+      {
+        const pageToken = query[spec.pageTokenParamInQuery ?? 'pageToken'];
+        // console.log('serving pageToken', pageToken);
+        let page = 1;
+        if (!pageToken) {
+          numItems = 10;
+          hasMore = true;
+        } else if (pageToken === 'token-2') {
+          page = 2;
+          numItems = 10;
+          hasMore = true;
+        } else if (pageToken === 'token-3') {
+          page = 3;
+          numItems = 5;
+        } else {
+          numItems = 0;
+        }
+        if (hasMore) {
+          const nextPageToken = `token-${page + 1}`;
+          body[spec.pageTokenParamInResponse ?? 'nextPageToken'] =
+            nextPageToken;
+          // console.log('next page token', nextPageToken);
+        } else {
+          body[spec.pageTokenParamInResponse ?? 'nextPageToken'] = null;
+          // console.log('no next page token');
+        }
       }
-      if (hasMore) {
-        const nextPageToken = `token-${page + 1}`;
-        body[spec.pageTokenParamInResponse ?? 'nextPageToken'] = nextPageToken;
-        // console.log('next page token', nextPageToken);
-      } else {
-        body[spec.pageTokenParamInResponse ?? 'nextPageToken'] = null;
-        // console.log('no next page token');
-      }
-    }
-    break;
+      break;
     default: {
       numItems = 10;
     }
