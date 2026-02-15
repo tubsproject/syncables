a64f37de72f9719fe2a8d10dba2952b9c56079e2
Michiel de Jong <michiel@pondersource.com>
2026-02-15 17:46:55 +0800

    fetch-cache working

--- src/readme-example.ts
+++ src/readme-example.ts
@@ -1,4 +1,6 @@
 import { readFileSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
+import { createHash } from 'crypto';
 import { components } from './types/google-calendar.js';
 import { Syncable } from './syncable.js';
 
@@ -13,7 +15,28 @@ const syncable = new Syncable<Entry>({
   authHeaders: {
     Authorization: `Bearer ${process.env.GOOGLE_BEARER_TOKEN}`,
   },
+  fetchFunction: async (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+    console.log('Fetch called with args:', input, init);
+    const data = JSON.stringify([input, init]);
+    const hash = createHash('sha256')
+                   .update(data)
+                   .digest('hex');
+    try {
+      const cached = await readFile(`./.fetch-cache/${hash}.json`);
+      console.log('using cached response for', input);
+      return new Response(cached, { status: 200 });
+    } catch (err) {
+      void err;
+      const fetched = await fetch(input, init);
+      const text = await fetched.text();
+      await writeFile(`./.fetch-cache/${hash}.json`, text);
+      console.log('cached response for', input);
+      return new Response(text, { status: fetched.status, headers: fetched.headers });
+    }
+    return fetch(input, init);
+  },
   dbConn: 'postgresql://syncables:syncables@localhost:5432/db_unit_tests?sslmode=disable',
 });
 const data = await syncable.fullFetch();
-console.log(data);
+void data;
+// console.log(data);
