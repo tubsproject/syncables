d3b48c7a36172c1a2dad715fd53af50571eb9908
Michiel de Jong <michiel@pondersource.com>
2026-02-17 14:55:20 +0800

    Fix integration tests

--- __tests__/integration/expected/netfly.js
+++ __tests__/integration/expected/netfly.js
@@ -9,6 +9,7 @@ for (let i = 0; i < 9; i += 1) {
     "documentVesid": "string",
     "feedback": "string",
     "fileName": "string",
+    "flow": "out",
     "id": 1,
     "inputDate": "2026-02-10T02:52:04.729Z",
     "lastTry": "2026-02-10T02:52:04.729Z",
