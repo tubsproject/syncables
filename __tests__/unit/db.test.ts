14a2ea1b2eac9258cde3c51f4978b9fe2dc0982c
Michiel de Jong <michiel@pondersource.com>
2026-02-17 15:46:48 +0800

    remove console.log

--- __tests__/unit/db.test.ts
+++ __tests__/unit/db.test.ts
@@ -218,7 +218,7 @@ describe('getFields', () => {
       '/administrations{format}',
       [],
     );
-    console.log(fields);
+    // console.log(fields);
     expect(fields).toEqual({
       id: {
         type: 'string',
