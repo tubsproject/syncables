3b1be6b7d52158ded1fa4ca2fcf7541df2b7f966
Michiel de Jong <michiel@pondersource.com>
2026-03-05 12:54:22 +0800

    improve generate script (only exploration still)

--- src/generate.ts
+++ src/generate.ts
@@ -2,6 +2,21 @@ import { readFile, readdir, stat } from 'fs/promises';
 import { specStrToObj } from './utils.js';
 import { default as path } from 'path';
 
+// For all API endpoints that have offset and limit parameters, generate the following overlay:
+// openapi: 3.0.0
+// extends: ...
+// actions:
+// - target: components
+//   update:
+//     paginationSchemes:
+//       paginate: ...
+//       offset:
+//         parameter: offset
+//       pageSize:
+//         parameter: limit
+// my target is to get my own real personal data synced from many APIs
+// so I should refactor the code in this repo to use paginationSchemes
+
 async function isDirectory(path: string): Promise<boolean> {
   try {
     const statInfo = await stat(path);
@@ -67,7 +82,7 @@ async function processFileOrDir(filePath: string): Promise<void> {
 // - Google syncToken
 // - MoneyBird synchronization
 async function processFile(filename: string): Promise<void> {
-  console.log(`\nProcessing file: ${filename}`);
+  // console.log(`\nProcessing file: ${filename}`);
   const specStr = await readFile(filename, 'utf-8');
   let specObj;
   try {
@@ -76,34 +91,38 @@ async function processFile(filename: string): Promise<void> {
     console.error(`Error parsing specification: ${err}`);
     return;
   }
+  const found = {};
   Object.keys(specObj.paths).forEach((path) => {
     if (specObj.paths[path].get) {
-      console.log(`GET ${path}`);
+      // console.log(`GET ${path}`);
       if (specObj.paths[path].get.parameters) {
         specObj.paths[path].get.parameters.forEach((param) => {
-          if (['page', 'maxResults', 'pageToken', 'nextToken', 'syncToken'].includes(param.name)) {
-            console.log(`  ${param.name} looks like a pagination parameter`);
+          if (['page', 'maxResults', 'pageToken', 'nextToken', 'syncToken', 'offset', 'limit'].includes(param.name)) {
+            found[param.name] = true;
           }
         });
       }
-      Object.keys(specObj.paths[path].get.responses).forEach((statusCode) => {
-        if (statusCode.startsWith('2')) {
-          const response = specObj.paths[path].get.responses[statusCode];
-          if (response.content) {
-            Object.keys(response.content).forEach((contentType) => {
-              if (response.content[contentType].schema.type === 'array') {
-                console.log(`  Response ${statusCode} returns an array, which may indicate pagination`);
-              }
-            });
-          }
-          if (response.headers && response.headers.link) {
-            console.log(`  Response ${statusCode} has a Link header, which may indicate pagination`);
-            console.log(response.headers.link);
-          }
-        }
-      });
+      // Object.keys(specObj.paths[path].get.responses).forEach((statusCode) => {
+      //   if (statusCode.startsWith('2')) {
+      //     const response = specObj.paths[path].get.responses[statusCode];
+      //     if (response.content) {
+      //       Object.keys(response.content).forEach((contentType) => {
+      //         if (response.content[contentType].schema.type === 'array') {
+      //           console.log(`  Response ${statusCode} returns an array, which may indicate pagination`);
+      //         }
+      //       });
+      //     }
+      //     if (response.headers && response.headers.link) {
+      //       console.log(`  Response ${statusCode} has a Link header, which may indicate pagination`);
+      //       console.log(response.headers.link);
+      //     }
+      //   }
+      // });
     }
   });
+  if (Object.keys(found).length > 0) {
+    console.log(`${filename}: some GET endpoints have pagination parameters: ${Object.keys(found).join(', ')}`);
+  }
 }
 
 const baseDir = process.argv[2];
