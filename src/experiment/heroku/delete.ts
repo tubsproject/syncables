8bedf652da7b328fc9789a7fa1958ded940b3e8d
Michiel de Jong <michiel@pondersource.com>
2026-04-05 22:14:09 +0800

    remove outdated guard

--- src/experiment/heroku/delete.ts
+++ src/experiment/heroku/delete.ts
@@ -4,9 +4,6 @@ async function deleteItem(
   authHeaders: { [key: string]: string },
   itemId: string,
 ): Promise<void> {
-  if (itemId === 'a2516ec8-8e5e-48ae-b0cc-2051aab43893') {
-    throw new Error('do not delete this one, it is in actual use');
-  }
   const url = `${baseUrl}/${itemId}`;
   const result = await fetch(url, {
     method: 'delete',
